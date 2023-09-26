76fa86d99102c38ef9b7502cfbb7912b7ee0a663
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-26 19:03:41 +0300

    Initial commit

---
+++ src/main/java/org/planning/bootstrap/Lab8SpringApplication.java
@@ -0,0 +1,13 @@
+package org.planning.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab8SpringApplication.class, args);
+	}
+
+}
